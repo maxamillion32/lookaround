05553a5f97903c649b9e0e6afee34272b6c0e9dc
Alexander Parunov <alexander.parunov@gmail.com>
2016-10-05 20:39:49 +0300

    Added MainActivity,SignIn,SingUp  -Constructed layouts for sign-in and sing-up

---
+++ app/src/main/java/alexparunov/lookaround/accounts/SignIn.java
@@ -0,0 +1,16 @@
+package alexparunov.lookaround.accounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexparunov.lookaround.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+}
---
+++ app/src/main/java/alexparunov/lookaround/accounts/SignUp.java
@@ -0,0 +1,15 @@
+package alexparunov.lookaround.accounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexparunov.lookaround.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
