62b70ae44b97af98c58aecf77132f92ac8455bff
Alexander Parunov <alexander.parunov@gmail.com>
2016-10-07 18:55:59 +0300

    Restructed AuthMainActivity --Added fragments folder --Set up the OnItemClickedMenu

--- app/src/main/java/alexparunov/lookaround/authenticated/AuthMainActivity.java
+++ app/src/main/java/alexparunov/lookaround/authenticated/AuthMainActivity.java
@@ -2,15 +2,11 @@ package alexparunov.lookaround.authenticated;
 
 import android.app.Activity;
 
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,17 +17,14 @@ import alexparunov.lookaround.R;
 public class AuthMainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
+
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
     private CharSequence mTitle;
     private FirebaseAuth firebaseAuth;
 
+    private final int PROFILE_POSITION = 0;
+    private final int MY_EVENTS_POSITION = 1;
     private final int SIGNOUT_POSITION = 2;
 
     @Override
@@ -52,73 +45,29 @@ public class AuthMainActivity extends Activity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        if(position == SIGNOUT_POSITION) {
-            firebaseAuth.signOut();
-            startActivity(new Intent(AuthMainActivity.this, MainActivity.class));
-        }
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.activity_auth_main_container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
+        Fragment fragment = null;
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.drawer_title1);
+        switch (position) {
+            case PROFILE_POSITION:
                 break;
-            case 2:
-                mTitle = getString(R.string.drawer_title2);
+            case MY_EVENTS_POSITION:
+                break;
+            case SIGNOUT_POSITION:
+                firebaseAuth.signOut();
+                startActivity(new Intent(AuthMainActivity.this, MainActivity.class));
+                break;
+            default:
                 break;
         }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            transaction.replace(R.id.activity_auth_main_container, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+
     }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_auth_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((AuthMainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
