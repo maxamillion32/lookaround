9872a11426db66bea335ec617aaf5683cef31fc2
Alexander Parunov <alexander.parunov@gmail.com>
2016-10-19 19:09:08 +0300

    Added Repeat password field to ProfileFragment

--- app/src/main/java/alexparunov/lookaround/authenticated/fragments/ProfileFragment.java
+++ app/src/main/java/alexparunov/lookaround/authenticated/fragments/ProfileFragment.java
@@ -29,11 +29,13 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
   View view;
   private EditText etFullName;
   private EditText etEmail;
-  private EditText etPassword;
+  private EditText etPassword1;
+  private EditText etPassword2;
   private FirebaseUser firebaseUser;
   private String fullName;
   private String email;
-  private String password;
+  private String password1;
+  private String password2;
 
   public ProfileFragment() {
   }
@@ -62,7 +64,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     etFullName = (EditText) view.findViewById(R.id.fragment_profile_nameET);
     etEmail = (EditText) view.findViewById(R.id.fragment_profile_emailET);
-    etPassword = (EditText) view.findViewById(R.id.fragment_profile_passwordET);
+    etPassword1 = (EditText) view.findViewById(R.id.fragment_profile_newPassword1ET);
+    etPassword2 = (EditText) view.findViewById(R.id.fragment_profile_newPassword2ET);
     Button bSubmit = (Button) view.findViewById(R.id.fragment_profile_submitB);
 
 
@@ -82,17 +85,19 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         if (context == null)
           return;
 
-        String errorMessage1 = "Please enter valid email address";
-
-        String errorMessage2 = "Password should begin with letter, be 6-18 characters " +
+        final String errorMessage1 = "Please enter valid email address";
+        final String errorMessage2 = "Password should begin with letter, be 6-18 characters " +
             "long and must contain only letters, numbers, and underscore";
+        final String errorMessage3 = "Passwords should match";
 
         if (etFullName != null)
           fullName = etFullName.getText().toString().trim();
         if (etEmail != null)
           email = etEmail.getText().toString().trim();
-        if (etPassword != null)
-          password = etPassword.getText().toString().trim();
+        if (etPassword1 != null && etPassword2 != null) {
+          password1 = etPassword1.getText().toString().trim();
+          password2 = etPassword2.getText().toString().trim();
+        }
 
         UserProfileChangeRequest.Builder request = new UserProfileChangeRequest.Builder();
 
@@ -104,10 +109,18 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
         if (email != null) {
           if (!email.isEmpty()) {
-            if (AccountUtils.isValidEmail(email))
-              firebaseUser.updateEmail(email);
-            else
+            if (AccountUtils.isValidEmail(email)) {
+              firebaseUser.updateEmail(email)
+                  .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                      Toast.makeText(context,"Email was successfully updated",Toast.LENGTH_SHORT).show();
+                    }
+                  });
+            }
+            else {
               Toast.makeText(context, errorMessage1, Toast.LENGTH_SHORT).show();
+            }
           }
         }
 
@@ -124,13 +137,23 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
               }
             });
 
-        if (password != null) {
-          if (!password.isEmpty()) {
-            Log.d("PR", password);
-            if (AccountUtils.isValidPassword(password))
-              firebaseUser.updatePassword(password);
-            else
-              Toast.makeText(context, errorMessage2, Toast.LENGTH_SHORT).show();
+        if (password1 != null && password2 != null) {
+          if (!password1.isEmpty() && !password2.isEmpty()) {
+            if (password1.contentEquals(password2)) {
+              if (AccountUtils.isValidPassword(password1)) {
+                firebaseUser.updatePassword(password1)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                      @Override
+                      public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(context,"Password was successfully updated", Toast.LENGTH_SHORT).show();
+                      }
+                    });
+              } else {
+                Toast.makeText(context, errorMessage2, Toast.LENGTH_SHORT).show();
+              }
+            } else {
+              Toast.makeText(context,errorMessage3,Toast.LENGTH_SHORT).show();
+            }
           }
         }
         break;
